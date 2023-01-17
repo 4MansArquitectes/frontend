172b87744949b1ecf30f53a46533fce4872ca4a2
Andrea Pedreño <apedrenodiez@gmail.com>
2023-01-17 18:20:48 +0100

    Add global style files

---
+++ src/styles/globaStyleFonts.tsx
@@ -0,0 +1,14 @@
+const globalStyleFonts = {
+  mainFont: "montserrat, sans-serif",
+  secondaryFont: "roboto, sans-serif",
+  mainFontSize: "1rem",
+  mediumFontSize: "20px",
+  superFontSize: "25px",
+  smallFontSize: "0.9rem",
+  mainFontRegular: 400,
+  mainFontBold: 600,
+  secondaryFontRegular: 400,
+  secondaryFontBold: 700,
+};
+
+export default globalStyleFonts;
