de4478ab8f152b69408215862993eecfad6bc19e
Tutitoos <tutitoosdev@gmail.com>
2023-01-19 16:43:34 +0100

    Fix test component OurTeamCard

--- src/lib/teamMembers.ts
+++ src/lib/teamMembers.ts
@@ -3,12 +3,12 @@ import type { TeamMembers } from "../types/teamTypes";
 const teamMembers: TeamMembers = [
   {
     name: "Susanna Font Goméz",
-    tag: "Arquitecte",
+    tag: "Arquitecta",
     image: "https://i.imgur.com/4OigOzX.png",
   },
   {
     name: "Bego Rus",
-    tag: "Arquitecte",
+    tag: "Arquitecta",
     image: "https://i.imgur.com/4OigOzX.png",
   },
 ];
