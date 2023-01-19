2f6482e17fbe0c143ce6b2cf14b4c60fd039d592
Andrea Pedreño <apedrenodiez@gmail.com>
2023-01-19 15:36:46 +0100

    Included app styled file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,14 @@
 import Layout from "../Layout/Layout";
 import Router from "../Router/Router";
+import AppStyled from "./AppStyled";
 
 const App = (): JSX.Element => {
   return (
-    <>
+    <AppStyled>
       <Layout>
         <Router />
       </Layout>
-    </>
+    </AppStyled>
   );
 };
 
---
+++ src/components/App/AppStyled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+export default AppStyled;
