9adbab26a05bbee80b01138636f552c0a698f071
Tutitoos <tutitoosdev@gmail.com>
2023-01-27 17:24:49 +0100

    Add config rome

--- src/lib/teamMembers.ts
+++ src/lib/teamMembers.ts
@@ -1,24 +1,24 @@
 import type { TeamMembers } from "../types/teamTypes";
 
 const teamMembers: TeamMembers = [
-	{
-		index: 0,
-		name: "Susanna Font Goméz",
-		tag: "Arquitecta",
-		image: "https://i.imgur.com/4OigOzX.png",
-	},
-	{
-		index: 1,
-		name: "Begoña Rus Luis",
-		tag: "Arquitecta",
-		image: "https://i.imgur.com/4OigOzX.png",
-	},
-	{
-		index: 2,
-		name: "Emilia Clarke",
-		tag: "Arquitecta",
-		image: "https://i.imgur.com/4OigOzX.png",
-	},
+  {
+    index: 0,
+    name: "Susanna Font Goméz",
+    tag: "Arquitecta",
+    image: "https://i.imgur.com/4OigOzX.png",
+  },
+  {
+    index: 1,
+    name: "Begoña Rus Luis",
+    tag: "Arquitecta",
+    image: "https://i.imgur.com/4OigOzX.png",
+  },
+  {
+    index: 2,
+    name: "Emilia Clarke",
+    tag: "Arquitecta",
+    image: "https://i.imgur.com/4OigOzX.png",
+  },
 ];
 
 export default teamMembers;
